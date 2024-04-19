d5a335ccb57744e1859a24b9958403883b8e96b1
Sanghpal <kamblesanghpal2003@gmail.com>
2024-04-20 00:51:55 +0530

    Sole_Swap

---
+++ src/Components/Assets/All_products.js
@@ -0,0 +1,12 @@
+import p1_img from './dunk-low-shoes.webp'
+
+let all_product = [
+    {
+        id: 1,
+        name: 'dunk low shoes',
+        image: p1_img,
+        size:7,
+        last_bid: 83,
+        current_bid: 90
+    }
+]
